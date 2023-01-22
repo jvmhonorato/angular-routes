f1676bf761b4e9317f8f9bc7b9342048f21540d6
vituhonorato <vituhonorato.89@gmail.com>
2023-01-22 10:25:46 -0300

    replacing href with routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <app-menu></app-menu>
 <router-outlet></router-outlet>
   `
 
 })
 export class AppComponent {
-  
+
 }
