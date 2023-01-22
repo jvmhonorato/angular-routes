d2962c8b6acea47ac58f73403e18cc7e0ea32ec6
vituhonorato <vituhonorato.89@gmail.com>
2023-01-22 09:51:03 -0300

    creating pages and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+//pages
+import { HomeComponent } from './pages/home/home.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+
 const routes: Routes = [
-  {path:"",
-  component: PageHomeComponent,
-  pathMatch: 'full'
-}
+  {path:"",component: HomeComponent,pathMatch: 'full'},
+  {path:"sobre",component: SobreComponent},
+  {path:"404",component: PageErrorComponent},
 ];
 
 @NgModule({
