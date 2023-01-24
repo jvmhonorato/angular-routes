c5cb604eec5a7465f8890aee26085dc84313a4d2
vituhonorato <vituhonorato.89@gmail.com>
2023-01-24 20:52:04 -0300

    add new component dashbord

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
