bf1b547b0b41919b1831b6c89f559aaef1b64654
vituhonorato <vituhonorato.89@gmail.com>
2023-01-24 22:07:22 -0300

    add independent routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path:"sobre",component: SobreComponent, children: [
     {path:"victor",component: SobreComponent},
   ]},
-
+  {path:"dashboard",loadChildren:() => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
   {path:"404",component: PageErrorComponent},
   {path:"**",redirectTo:"404"},
 ];
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule
   ]
 })
 export class DashboardModule { }
